3fd568b412830508d5bc0452dc2cb6c668b37dc2
iakash2604 <iakash2702@gmail.com>
2019-02-11 11:00:15 +0530

    resolve image display issues

--- webapp/app.py
+++ webapp/app.py
@@ -43,24 +43,28 @@ def data():
 @app.route('/grid')
 def grid():
     # paths are extracted from session. these were collected in data.html
-    paths = session['paths']
-    p_path_input = paths[0]
-    q_path_input = paths[1]
-    pij_path_input = paths[2]
-    qij_path_input = paths[3]
+    if('paths' in session.keys()):
+        paths = session['paths']
+        p_path_input = paths[0]
+        q_path_input = paths[1]
+        pij_path_input = paths[2]
+        qij_path_input = paths[3]
 
-    # read data
-    p = np.load(p_path_input)
-    q = np.load(q_path_input)
-    pij = np.load(pij_path_input)
-    qij = np.load(qij_path_input)
+        # read data
+        p = np.load(p_path_input)
+        q = np.load(q_path_input)
+        pij = np.load(pij_path_input)
+        qij = np.load(qij_path_input)
+        
+        # perform state estimation
+        vse, phise = state_estimate_alt("case14.mat", p, q, pij, qij, 1.1, 0.9)
+        
+        # generate graph using vse, phise pass the path of the graph created to render_template
+        graph_path = "/home/akash/github/EMS/webapp/bus_grid.jpg"
+        return render_template('grid.html', buses=buses, graph_path = graph_path)
     
-    # perform state estimation
-    vse, phise = state_estimate_alt("case14.mat", p, q, pij, qij, 1.1, 0.9)
-    
-    # generate graph using vse, phise pass the path of the graph created to render_template
-    graph_path = "/home/akash/github/EMS/webapp/bus_grid.jpg"
-    return render_template('grid.html', buses=buses, graph_path = graph_path)
+    else:
+        return "please enter paths first"
 
 
 if(__name__ == '__main__'):

c7d17d144f4c8ec82d8b5e7f1c3259c71994baef
Pranav Verma <pranav.verma97@gmail.com>
2019-02-11 20:16:49 +0530

    added no_path_SE functionality

--- webapp/app.py
+++ webapp/app.py
@@ -40,7 +40,16 @@ def operations():
 @app.route('/state_estimation')
 @nocache
 def state_estimation():
-    return render_template('state_estimation.html', buses=buses)
+    global init_time
+    graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.png".rstrip()
+    if init_time==0 or (time.time()-init_time)>=300.0:
+        init_time=time.time()
+        graph_path=se()
+        init_time=time.time()
+        return render_template('grid.html', buses=buses, graph_path = graph_path)
+    else:
+        return render_template('grid.html', buses=buses, graph_path = graph_path)
+    # return render_template('state_estimation.html', buses=buses)
 
 @app.route('/readme')
 @nocache
@@ -69,35 +78,35 @@ def grid():
     global init_time
     # paths are extracted from session. these were collected in data.html
     if('paths' in session.keys()):
-        # if init_time==0 or (time.time()-init_time)>=0.0:
-        init_time=time.time()
-        #do state estimation again
-        paths = session['paths']
-        p_path_input = paths[0]
-        q_path_input = paths[1]
-        pij_path_input = paths[2]
-        qij_path_input = paths[3]
+        if init_time==0 or (time.time()-init_time)>=300.0:
+            init_time=time.time()
+            #do state estimation again
+            paths = session['paths']
+            p_path_input = paths[0]
+            q_path_input = paths[1]
+            pij_path_input = paths[2]
+            qij_path_input = paths[3]
 
-        # read data
-        p = np.load(p_path_input)
-        q = np.load(q_path_input)
-        pij = np.load(pij_path_input)
-        qij = np.load(qij_path_input)
-        
-        # perform state estimation
-        print(p,q)
-        vse, phise = state_estimate_alt("case14.mat", p, q, pij, qij, 1.1, 0.9)
-        
-        # generate graph using vse, phise pass the path of the graph created to render_template
-        graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.png".rstrip()
-        draw_graph(pij,qij,vse,phise,graph_path)
-        init_time=time.time()
-        return render_template('grid.html', buses=buses, graph_path = graph_path)
-        # else:
-        #     #SE done <5 mins ago, just render the old image
-        #     print("Last SE less than 5 mins ago. Rendering old image.")
-        #     graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.jpg".rstrip()
-        #     return render_template('grid.html', buses=buses, graph_path = graph_path)
+            # read data
+            p = np.load(p_path_input)
+            q = np.load(q_path_input)
+            pij = np.load(pij_path_input)
+            qij = np.load(qij_path_input)
+            
+            # perform state estimation
+            print(p,q)
+            vse, phise = state_estimate_alt("case14.mat", p, q, pij, qij, 1.1, 0.9)
+            
+            # generate graph using vse, phise pass the path of the graph created to render_template
+            graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.png".rstrip()
+            draw_graph(pij,qij,vse,phise,graph_path)
+            init_time=time.time()
+            return render_template('grid.html', buses=buses, graph_path = graph_path)
+        else:
+            #SE done <5 mins ago, just render the old image
+            print("Last SE less than 5 mins ago. Rendering old image.")
+            graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.jpg".rstrip()
+            return render_template('grid.html', buses=buses, graph_path = graph_path)
 
     else:
         return "please enter paths first"
--- webapp/functions.py
+++ webapp/functions.py
@@ -1,18 +1,22 @@
 import numpy as np
-# import matlabopti
-# import emsmatlab
+import os
+import pandas
+import re
+import time
+
 import matlab.engine
 
 import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
+import pygraphviz as pgv
+
 import matplotlib.pyplot as plt
 from pypower import case14
-from networkx.drawing.layout import _sparse_fruchterman_reingold
-import numpy
-import networkx as nx
+
 import pylab as plt
-from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
-# !pip install pygraphviz
-import pygraphviz as pgv
+
+import socket
+import OpenOPC
 
 
 baseMVA=100.0
@@ -501,3 +505,84 @@ def draw_graph(Pline,Qline,Vbus,phibus,filename='graph.png'):
     print(A)
     A.layout('dot')
     A.draw(filename)
+
+def se(base='/Users/pranav97/Downloads/EMS-master/webapp/data'):
+    # base='C:\\Users\\Administrator\\Downloads\\akash_pranav2\\final_data'
+    TCP_IP = '127.0.0.1'
+    TCP_PORT = 4575
+    BUFFER_SIZE = 1024
+
+    load_bus=[2,3,4,5,6,9,10,11,12,13,14]
+    gen_bus=[1, 2, 3, 6, 8]
+
+    # print(load_bus)
+    pload_tags=['Pset'+str(i) for i in load_bus]
+    qload_tags=['Qset'+str(i) for i in load_bus]
+    rtds_tags=[]
+    rtds_tags.extend(pload_tags)
+    rtds_tags.extend(qload_tags)
+    rtds_tags.append('P_BUS2x1')
+
+    nyiso_tags=[str(i) for i in load_bus]
+    nyiso_tags.extend(['q'+str(i) for i in load_bus])
+    nyiso_tags.append('P_BUS2x1')
+
+    nyiso2rtds=dict(zip(nyiso_tags,rtds_tags))
+    # print(nyiso2rtds)
+
+    # read_tags=['P'+str(i) for i in gen_bus]
+    # read_tags.extend(['Q'+str(i) for i in gen_bus])
+    read_tags=[]
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((TCP_IP, TCP_PORT))
+
+    opc=OpenOPC.open_client('localhost')
+    opc.connect('Kepware.KEPServerEX.V6','localhost')
+
+    for file in os.listdir(base):
+        df=pandas.read_csv(os.path.join(base,file))
+        rows=[]
+        for i in df.index:
+            row_dict={}
+            for k in df.keys():
+                row_dict[k]=df[k][i]
+            '''
+            write data:
+            '''
+            for k in nyiso_tags:
+                val2write=numpy.array(df[k][i]).tolist()
+                if k=='P_BUS2x1':
+                    str1='SetSlider "Subsystem #1 : CTLs : Inputs : P_BUS2x1" = '+str(val2write)+';'
+                else:
+                    bus=int((filter(str.isdigit, nyiso2rtds[k])))
+                    cm="Subsystem #1 : Loads : DL"+str(bus)+" : "+str(nyiso2rtds[k])
+                    str1='SetSlider "'+str(cm)+'" = '+str(val2write)+';' 
+                print str1
+                s.send(str1)
+                s.send('SUSPEND 0.2;')
+                s.send('ListenOnPortHandshake(temp_string);')
+                tokenstring = s.recv(BUFFER_SIZE)
+                # print "The token string returned is: ", tokenstring            
+            '''
+            Wait for system to stabilise
+            '''
+            time.sleep(10)
+            
+            '''
+            read data
+            '''
+            p,q,pij,qij=read_data(opc)
+            v,phi=eng.state_estimate('case14.mat',p,q,pij,qij)
+            graph_path = "/Users/pranav97/Downloads/EMS-master/webapp/static/bus_grid.png".rstrip()
+            draw_graph(pij,qij,v,phi,filename=graph_path)
+            break
+        break
+
+
+    s.send('ClosePort(%d);' % (TCP_PORT))
+    s.close()
+
+    print 'All iterations complete.'
+    return graph_path
